bd2009e1c54c4c1a97a5ef3fc59a1700403e291a
Auzzzz <s3544232@student.rmit.edu.au>
2020-03-19 02:37:38 +0000

    Monitor adding temp

--- monitorAndDisplay.py
+++ monitorAndDisplay.py
@@ -1,17 +1,25 @@
 import json
+import time
 from sense_hat import SenseHat
 #https://pythonbasics.org/read-json-file/
 
+sense = SenseHat()
+#temp = sense.get_temperature()
+#JSON read
+#with open('config.json', 'r') as myfile:
+#    data=myfile.read()
+
+#obj = json.loads(data)
+
+#JSON test print
+#print("cold_max: " + str(obj['cold_max']))
+#print("comfortable_min: " + str(obj['comfortable_min']))
+#print("comfortable_max: " + str(obj['comfortable_max']))
 
 
-
-
-
-with open('config.json', 'r') as myfile:
-    data=myfile.read()
-
-obj = json.loads(data)
-
-print("cold_max: " + str(obj['cold_max']))
-print("comfortable_min: " + str(obj['comfortable_min']))
-print("comfortable_max: " + str(obj['comfortable_max']))
+def temp():
+	sense.show_message("test")
+        time.sleep(5)
+#	sense.show_message("Temp: %s C" % temp)
+#	print("Temperature: %s C" % temp)
+#	print(sense.temp)
