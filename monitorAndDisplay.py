dab83bdd41a6152a6d12fe5de872d0c1865a879d
Auzzzz <s3544232@student.rmit.edu.au>
2020-03-19 07:42:11 +0000

    update

--- monitorAndDisplay.py
+++ monitorAndDisplay.py
@@ -1,25 +1,29 @@
+
 import json
 import time
 from sense_hat import SenseHat
-# https://pythonbasics.org/read-json-file/
+#https://pythonbasics.org/read-json-file/
 
+#JSON read
+with open('config.json', 'r') as myfile:
+    data=myfile.read()
+
+obj = json.loads(data)
+
+#JSON test print
+print("cold_max: " + str(obj['cold_max']))
+print("comfortable_min: " + str(obj['comfortable_min']))
+print("comfortable_max: " + str(obj['comfortable_max']))
+
+
+#
 sense = SenseHat()
-#temp = sense.get_temperature()
-# JSON read
-# with open('config.json', 'r') as myfile:
-#    data=myfile.read()
+temp = sense.get_temperature()
+print("Temperature: %s C" % temp)
 
-#obj = json.loads(data)
+#sense.clear(255,255,0)
 
-# JSON test print
-#print("cold_max: " + str(obj['cold_max']))
-#print("comfortable_min: " + str(obj['comfortable_min']))
-#print("comfortable_max: " + str(obj['comfortable_max']))
+#Text display not needed =(
+#sense.show_message("Temp = %s C " % temp)
 
 
-def temp():
-    sense.show_message("test")
-    time.sleep(5)
-    sense.show_message("Temp: %s C" % temp)
-    print("Temperature: %s C" % temp)
-    print(sense.temp)
