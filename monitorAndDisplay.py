7a61960351f20c6f4a2bf85cfc281d9023d43758
Auzzzz <s3544232@student.rmit.edu.au>
2020-03-19 02:25:24 +0000

    Started monitoranddisplay

--- monitorAndDisplay.py
+++ monitorAndDisplay.py
@@ -1,4 +1,17 @@
 import json
+from sense_hat import SenseHat
+#https://pythonbasics.org/read-json-file/
 
-with open("config.json", "r") as read_file:
-    data = json.load(read_file)
+
+
+
+
+
+with open('config.json', 'r') as myfile:
+    data=myfile.read()
+
+obj = json.loads(data)
+
+print("cold_max: " + str(obj['cold_max']))
+print("comfortable_min: " + str(obj['comfortable_min']))
+print("comfortable_max: " + str(obj['comfortable_max']))
