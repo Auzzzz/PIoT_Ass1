729f59c34d3a037fd8c4f7306918b496558e6eb6
Auzzzz <s3544232@student.rmit.edu.au>
2020-03-26 15:07:54 +1100

    C1

--- electronicDie.py
+++ electronicDie.py
@@ -1,1 +1,16 @@
-print("Test")
+#https://projects.raspberrypi.org/en/projects/getting-started-with-the-sense-hat/8
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+while True:
+	acceleration = sense.get_accelerometer_raw()
+	x = acceleration['x']
+	y = acceleration['y']
+	z = acceleration['z']
+
+	x=round(x, 0)
+	y=round(y, 0)
+	z=round(z, 0)
+
+	print("x={0}, y={1}, z={2}".format(x, y, z))
