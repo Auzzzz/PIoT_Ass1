cf30b91540ec45478706b0ab2a39c5383416248a
Auzzzz <s3544232@student.rmit.edu.au>
2020-03-27 04:01:58 +0000

    Displays Number & Roll Works

--- electronicDie.py
+++ electronicDie.py
@@ -1,17 +1,18 @@
+
 #https://projects.raspberrypi.org/en/projects/getting-started-with-the-sense-hat/8
 from sense_hat import SenseHat
 
 sense = SenseHat()
 
 while True:
-	acceleration = sense.get_accelerometer_raw()
-    
-	x = acceleration['x']
-	y = acceleration['y']
-	z = acceleration['z']
-    
-    if x > 2:
-        sense.show_message("1")
+	x, y, z = sense.get_accelerometer_raw().values()
+
+	x = abs(x)
+	y = abs(y)
+	z = abs(z)
+
+	if x > 2 or y > 2 or z > 2:
+        	sense.show_message("1")
 
 	#x=round(x, 0)
 	#y=round(y, 0)
