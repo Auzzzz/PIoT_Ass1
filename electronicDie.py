2dbb6d2ad44812caa605d18beec0ec7c6653c261
Auzzzz <s3544232@student.rmit.edu.au>
2020-03-27 15:37:44 +1100

    def added

--- electronicDie.py
+++ electronicDie.py
@@ -4,21 +4,22 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-while True:
-	x, y, z = sense.get_accelerometer_raw().values()
+x, y, z = sense.get_accelerometer_raw().values()
 
-	x = abs(x)
-	y = abs(y)
-	z = abs(z)
+x = abs(x)
+y = abs(y)
+z = abs(z)
 
+def getdice():
 	if x > 2 or y > 2 or z > 2:
         	sense.show_message("1")
+
+while True:
+	
+	getdice()
 
 	#x=round(x, 0)
 	#y=round(y, 0)
 	#z=round(z, 0)
 	#print("x={0}, y={1}, z={2}".format(x, y, z))
-
-
-	#Test
 
