3e304a29fab8d77b35bc5e0d2ab5ed1994f8526b
Auzzzz <s3544232@student.rmit.edu.au>
2020-03-27 15:48:18 +1100

    A3

--- electronicDie.py
+++ electronicDie.py
@@ -2,9 +2,11 @@
 #https://projects.raspberrypi.org/en/projects/getting-started-with-the-sense-hat/8
 from sense_hat import SenseHat
 import time
+import random
+
 sense = SenseHat()
 count = 0
-
+number = 0
 def dice():
 	x, y, z = sense.get_accelerometer_raw().values()
 
@@ -13,8 +15,8 @@ def dice():
 	z = abs(z)
 
 	if x > 2 or y > 2 or z > 2:
-        	sense.show_message("1")
-	
+		number = random(1,6)
+		print(number)
 
 while True:
 	dice()
